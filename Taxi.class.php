5065b9122db41c469765cbfe2e703ff2e8dc373d
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-04-15 06:44:12 +0900

    課題提出1 (strvalの出番は…？)

---
+++ Taxi.class.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . "/Car.class.php";
+
+class Taxi extends Car
+{
+    private $passenger;
+
+    public function pickUp($passenger)
+    {
+        $this->passenger = $passenger;
+        echo $passenger . "人乗車しました\n";
+    }
+    public function lower($passenger)
+    {
+        if ($this->passenger >= $passenger) {
+            $this->passenger -= $passenger;
+            echo $passenger . "人降車しました\n";
+        } else {
+            echo $passenger . "人は降車できません\n";
+        }
+        
+    }
+
+    public function information()
+    {
+        return
+            '車の車種:' . $this->name . "\n" .
+            '車体番号:' . $this->number . "\n" .
+            'カラー:' . $this->color . "\n" .
+            '乗車人数:' . $this->passenger . "人\n";
+    }
+
+}
+
+?>
