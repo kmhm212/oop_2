aa06f0f14981721bfdad4ce0d53563fc5cfa4bcc
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-04-18 14:07:40 +0900

    課題提出3

--- Taxi.class.php
+++ Taxi.class.php
@@ -9,30 +9,24 @@ class Taxi extends Car
     public function pickUp($passenger)
     {
         $this->passenger += $passenger;
-        return $passenger . "人乗車しました\n";
-    
-        // strval を使う場合は
-        // return strval($passenger) . "人乗車しました\n";
-        // と書くということですか？
+        return strval($passenger) . "人乗車しました\n";
     }
     public function lower($passenger)
     {
         if ($this->passenger >= $passenger) {
             $this->passenger -= $passenger;
-            return $passenger . "人降車しました\n";
+            return strval($passenger) . "人降車しました\n";
         } else {
-            return $passenger . "人は降車できません\n";
+            return strval($passenger) . "人は降車できません\n";
         }
     }
     public function information()
     {
         return
             '車の車種:' . $this->name . "\n" .
-            '車体番号:' . $this->number . "\n" .
+            '車体番号:' . strval($this->number) . "\n" .
             'カラー:' . $this->color . "\n" .
-            '乗車人数:' . $this->passenger . "人\n";
+            '乗車人数:' . strval($this->passenger) . "人\n";
     }
 }
-
-
 ?>
